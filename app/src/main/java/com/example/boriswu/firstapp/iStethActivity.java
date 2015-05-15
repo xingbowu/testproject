3253bed46f7b039af7d75dcc0722eb82afc6e87e
boriswu <sdau14@163.com>
2015-05-15 20:44:33 +0800

    my commit message

---
+++ app/src/main/java/com/example/boriswu/firstapp/iStethActivity.java
@@ -0,0 +1,17 @@
+package com.example.boriswu.firstapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class iStethActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_i_steth);
+    }
+
+}
